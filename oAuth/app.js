b9e3cbd943b5b279cad0537119ceedd79f9773ea
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-07-24 02:02:26 +0600

    added login/logout routes

--- oAuth/app.js
+++ oAuth/app.js
@@ -1,15 +1,19 @@
-const express = require('express')
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
-const app = express()
+const app = express();
 
-//set up view engine
-app.set('view engine', 'ejs')
+// set view engine
+app.set('view engine', 'ejs');
 
-//create home route
+// set up routes
+app.use('/auth', authRoutes);
+
+// create home route
 app.get('/', (req, res) => {
-  res.render('home')
-})
+    res.render('home');
+});
 
-app.listen(5000, () => {
-  console.log('App listening to port 5000')
-})
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
