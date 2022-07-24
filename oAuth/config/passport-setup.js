fa01d992b1ae2e185671250d2e5cf83987a2b84c
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-07-24 23:33:47 +0600

    added keys for google auth

--- oAuth/config/passport-setup.js
+++ oAuth/config/passport-setup.js
@@ -1,15 +1,13 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./keys');
 
 passport.use(
-  new GoogleStrategy(
-    {
-      // options for google strategy
-      // clientID: /* add your client ID here */,
-      // clientSecret: /* add your client secret here */
-    },
-    () => {
-      // passport callback function
-    }
-  )
-)
+    new GoogleStrategy({
+        // options for google strategy
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, () => {
+        // passport callback function
+    })
+);
