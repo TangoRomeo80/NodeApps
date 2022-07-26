4b84a28f41764e9bb49b0374f7cba7a1ff269b70
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2022-07-26 14:58:34 +0600

    added user saving functionality

--- oAuth/config/passport-setup.js
+++ oAuth/config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('./keys')
+const User = require('../models/user-model')
 
 passport.use(
   new GoogleStrategy(
@@ -14,6 +15,14 @@ passport.use(
       // passport callback function
       console.log('passport callback function fired')
       console.log(profile)
+      new User({
+        googleId: profile.id,
+        username: profile.displayName,
+      })
+        .save()
+        .then((newUser) => {
+          console.log('new user created: ', newUser)
+        })
     }
   )
 )
